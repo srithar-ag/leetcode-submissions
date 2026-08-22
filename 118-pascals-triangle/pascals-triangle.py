732a6614d0c85898d23a91902d87e10ef12fb6a7
Padma Sritharan A G <sritharag17@gmail.com>
2026-08-22 23:41:50 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (29.23%) - LeetSync

---
+++ 118-pascals-triangle/pascals-triangle.py
@@ -0,0 +1,12 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        triangle = [[1]]
+        for row_index in range(numRows - 1):
+            current_row = [1]
+            previous_row = triangle[-1]
+            for j in range(len(previous_row) - 1):
+                current_row.append(previous_row[j] + previous_row[j + 1])
+            current_row.append(1)
+            triangle.append(current_row)
+      
+        return triangle
